4f93773349d99ba38d8d073d13f8ba8b3fcb25dd
Basit Nawaz <basitnawaz507@gmail.com>
2022-05-19 12:05:13 +0500

    Playing with react tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("Render link in App", () => {
+test("App Component rendered", () => {
+	render(<App />);
+});
+
+test("Link is Correct", () => {
 	render(<App />);
 
 	const link = screen.getByTestId("link");
-	expect(link.href).toBe("https://google.com");
+	expect(link.href).toContain("https://google.com");
 });
