ab5412f3567e9a22651ffa08b010b70f4e424b7e
Basit Nawaz <basitnawaz507@gmail.com>
2022-05-19 11:52:10 +0500

    Test cases implemented

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("Render link in App", () => {
+	render(<App />);
+
+	const link = screen.getByTestId("link");
+	expect(link.href).toBe("https://google.com");
 });
